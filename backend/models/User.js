0795ee2f34b2216b2411eba57f3391b78b0da2b9
ballinpeanut <miltonmark.mh@gmail.com>
2025-10-21 20:34:17 -0500

    Created User schema using mongoose

---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            maxlength: 50
+        },
+
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            maxlength: 50
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        created_at: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+
+        last_login: {
+            type: Date,
+            required: true
+        }
+    },
+    { collection: "Users"}
+)
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
