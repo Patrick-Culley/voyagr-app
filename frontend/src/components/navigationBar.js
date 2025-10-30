5423175faa2637c8f154c629b2308762ab7fa507
amndaone <onea@oregonstate.edu>
2025-10-30 12:09:58 -0700

    Updates to UI: search bar centered, adjust positioning of register/login links

--- frontend/src/components/navigationBar.js
+++ frontend/src/components/navigationBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavigationBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light px-3 position-relative">
         <div className="container">
             <Link className="navbar-brand" to="/">Voyagr</Link>
             <div className="navbar-nav me-auto">
@@ -12,19 +12,27 @@ function NavigationBar() {
                     <Link className="nav-link" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/register">Register</Link>
-                </li>
-                <li className="nav-item">
                     <Link className="nav-link" to="/trips">My Trips</Link>
                 </li>
             </div>
+            {/* SEARCH BAR */}
+            <form className="d-flex position-absolute top-30 start-50 translate-middle-x" role="search">
+                <input class="form-control me-2"
+                type="search"
+                placeholder="Bon Voyage!"
+                aria-label="Search"
+                style={{ width: "400px "}}/>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </div>
-
-        {/* SEARCH BAR */}
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Bon Voyage!" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div className="navbar-nav ms-auto">
+            <li className="nav-item">
+                <Link className="nav-link" to="/register">Register</Link>
+            </li>
+            <li className="nav-item" to="/login">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+        </div>
         </nav>
     );
 }
