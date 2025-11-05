6f3bc6aef09cfc9a789360a01ebc8f8d71f5ac22
akaturtle2 <leviaka10@gmail.com>
2025-11-05 22:09:22 +0500

    Modified server.js file, it was missing errorHandler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 
 connectDb();
 const app = express();
@@ -10,11 +11,13 @@ const port = process.env.PORT || 5555;
 app.use(express.json());
 
 // allow React frontend to access routes
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 // Routes:
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/experiences", require("./routes/experienceRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
