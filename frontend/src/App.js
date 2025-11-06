57b220911ea4af821124b307f1d14e1a41d11d13
amndaone <onea@oregonstate.edu>
2025-11-05 20:32:17 -0800

    User login UI and form submission

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserRegistration from "./components/userRegistration";
 import NavigationBar from "./components/navigationBar";
 import Trips from "./pages/myTrips"
+import UserLogin from "./pages/userLogin";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         {/* <Route path="/" element={<Home/>} /> */}
         <Route path="/register" element={<UserRegistration/>} />
         <Route path="/trips" element={<Trips/>} />
+        <Route path="/login" element={<UserLogin/>} />
       </Routes>
     </Router>
   );
